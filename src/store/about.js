2a25972489ba5f7963e729b62112ff2b4c79c4d2
Suhyxn <momentomori21489@gmail.com>
2022-07-17 23:49:46 +0900

    Chore: About.js

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SUHYXN',
+    email: 'momentomori21489@gmail.com',
+    blog: 'https://velog.io/@momentomori',
+    phone: '+82-10-1234-1234',
+    image: 'https://avatars.githubusercontent.com/u/89029699?v=4'
+  })
+}
