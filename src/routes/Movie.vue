81fc5a0d1c7831ac40d5d43645672ea2b7b4558f
Suhyxn <momentomori21489@gmail.com>
2022-07-19 16:28:40 +0900

    Perf: mapState Vuex Helpers

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -76,7 +76,7 @@
 </template>
 
 <script>
-// import { METHOD_TYPES } from '@babel/types'
+import { mapState } from 'vuex'
 import Loader from '~/components/Loader'
 
 export default {
@@ -89,16 +89,15 @@ export default {
     }
   },
   computed: {
-    theMovie() {
-      return this.$store.state.movie.theMovie
-    },
-    loading() {
-      return this.$store.state.movie.loading
-    }
+    ...mapState('movie', [
+      'theMovie',
+      'loading'
+    ])
   },
   created() {
     console.log(this.$route)
     this.$store.dispatch('movie/searchMovieWithId', {
+      // movie/tt123762
       id: this.$route.params.id
     })
   },
