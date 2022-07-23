d5755a66b0ce9f19293046b494116eb329ee0866
Suhyxn <momentomori21489@gmail.com>
2022-07-23 22:43:00 +0900

    Study VTU TEST

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,18 +1,14 @@
-import axios from 'axios'
-import { fetchMovieTitle } from './example'
+import { mount } from '@vue/test-utils'
+import Example from './Example.vue'
 
-describe('비동기테스트', () => {
-  test('영화 제목 변환', async () => {
-    axios.get = jest.fn(() => {
-      return new Promise(resolve => {
-        resolve({
-          data: {
-            Title: 'Frozen II'
-          }
-        })
-      }) 
-    })
-    const title = await fetchMovieTitle()
-    expect(title).toBe('Frozen ii')
+test('메시지를 변경합니다', async () => {
+  const wrapper = mount(Example)
+  // wrapper.vm === this
+  expect(wrapper.vm.msg).toBe('Hello Vue test utils!')
+  // wrapper.vm.msg = 'Hello SUHYXN!'
+  await wrapper.setData({
+    msg: 'Hello SUHYXN!'
   })
+  expect(wrapper.vm.msg).toBe('Hello SUHYXN!')
+  expect(wrapper.find('div').text()).toBe('Hello SUHYXN!')
 })
